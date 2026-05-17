51f566b4f6da7fea2d8288fc0ef32a93f5bea0bb
Noctua76 <iliaskalivas@hotmail.com>
2026-05-17 18:04:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,30 @@
 const CACHE_NAME = "aegis-link-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./site.webmanifest",
-  "./android-chrome-192x192.png",
-  "./android-chrome-512x512.png",
-  "./apple-touch-icon.png"
-];
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 
-self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./site.webmanifest",
+        "./apple-touch-icon.png",
+        "./android-chrome-192x192.png",
+        "./android-chrome-512x512.png",
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
